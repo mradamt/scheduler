78a588ba8fbd014990956f87a556af4e868942d6
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-07 17:02:22 -0800

    Configure DayList to return a list of DayListItems

--- stories/index.js
+++ stories/index.js
@@ -7,6 +7,8 @@ import "index.scss";
 
 import Button from "components/Button";
 import DayListItem from "components/DayListItem";
+import DayList from "components/DayList";
+
 
 storiesOf("Button", module)
   .addParameters({
@@ -34,3 +36,43 @@ storiesOf("DayListItem", module) // Initiates Storybook and registers our DayLis
   .add("Clickable", () => (
     <DayListItem name="Tuesday" setDay={action("setDay")} spots={5} /> // action() allows us to create a callback that appears in the actions panel when clicked
   ));
+
+  const days = [
+    {
+      id: 1,
+      name: "Monday",
+      spots: 2,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      spots: 5,
+    },
+    {
+      id: 3,
+      name: "Wednesday",
+      spots: 0,
+    },
+    {
+      id: 4,
+      name: "Thursday",
+      spots: 5,
+    },
+    {
+      id: 5,
+      name: "Friday",
+      spots: 2,
+    },
+  ];
+  
+  storiesOf("DayList", module)
+    .addParameters({
+      backgrounds: [{ name: "dark", value: "#222f3e", default: true }],
+    })
+    .add("Monday", () => (
+      <DayList days={days} day={"Monday"} setDay={action("setDay")} />
+    ))
+    .add("Tuesday", () => (
+      <DayList days={days} day={"Tuesday"} setDay={action("setDay")} />
+    ));
+  
