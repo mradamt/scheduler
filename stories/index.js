8a10386305017a8de59a4304e8205413a5902520
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-08 19:58:33 -0800

    render stories for Appointment Empty and Booked

--- stories/index.js
+++ stories/index.js
@@ -154,6 +154,16 @@ storiesOf("Appointment", module)
   })
   .add("Appointment", () => <Appointment />)
   .add("Appointment with time", () => <Appointment time='12pm' />)
+  .add("Appointment Empty", () => (
+    <>
+      <Appointment id={1} time="12pm" />
+      <Appointment id="last" time="1pm" />
+    </>))
+  .add("Appointment Booked", () => (
+    <>
+      <Appointment id={1} time="12pm" interview={{ student: "Lydia Miller-Jones", interviewer }} />
+      <Appointment id="last" time="1pm" />
+    </>))  
   .add("Header", () => <Header time='12pm' />)
   .add("Empty", () => <Empty onAdd={action("onAdd")} />)
   .add("Show", () => <Show 
