2371a30d05a0e4fb14f24067673a79861a1cfb8a
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-08 20:29:09 -0800

    update Application.js with dummy data and render page itself

--- src/components/Application.js
+++ src/components/Application.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
-import "components/Application.scss";
 import DayList from "./DayList";
+import Appointment from './Appointment';
+
+import "components/Application.scss";
 
 const days = [
   {
@@ -30,6 +32,69 @@ const days = [
   },
 ];
 
+const appointments = [
+  {
+    id: 4,
+    time: "9am",
+    interview: {
+      student: "Dasher McQuade",
+      interviewer: {
+        id: 4,
+        name: "Mamoose Frindle",
+        avatar: "https://i.imgur.com/T2WwVfS.png",
+      }
+    }
+  },
+  {
+    id: 5,
+    time: "11am",
+  },
+  {
+    id: 6,
+    time: "11:30am",
+    interview: {
+      student: "Minus Langpow",
+      interviewer: {
+        id: 3,
+        name: "Noah Baarndoor",
+        avatar: "https://i.imgur.com/twYrpay.jpg",
+      }
+    }
+  },
+  {
+    id: 1,
+    time: "12pm",
+  },
+  {
+    id: 2,
+    time: "1pm",
+    interview: {
+      student: "Lydia Miller-Jones",
+      interviewer: {
+        id: 1,
+        name: "Sylvia Palmer",
+        avatar: "https://i.imgur.com/LpaY82x.png",
+      }
+    }
+  },
+  {
+    id: 3,
+    time: "4pm",
+    interview: {
+      student: "Bingbong Wazoo",
+      interviewer: {
+        id: 2,
+        name: "Ida Bazout",
+        avatar: "https://i.imgur.com/Nmx0Qxo.png",
+      }
+    }
+  }
+];
+
+
+
+/****************************************************************************/
+
 const Application = (props) => {
   const [day, setDay] = useState("Monday")
 
@@ -43,7 +108,7 @@ const Application = (props) => {
           />
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
-            <DayList days={days} day={day} setDay={setDay} />
+            <DayList values={days} day={day} setDay={setDay} />
           </nav>
           <img
             className="sidebar__lhl sidebar--centered"
@@ -52,7 +117,13 @@ const Application = (props) => {
           />
       </section>
       <section className="schedule">
-        {/* Replace this with the schedule elements durint the "The Scheduler" activity. */}
+        {appointments.map(appointment => (
+          <Appointment
+            key={appointment.id}
+            {...appointment}
+          />
+        ))}
+        {<Appointment key='last' time='5pm' />}
       </section>
     </main>
   );
