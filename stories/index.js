8fdae498bf5ed08d292c40117a6e7560f328d382
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-08 19:32:30 -0800

    add Form for Create and Edit, and stories

--- stories/index.js
+++ stories/index.js
@@ -17,6 +17,7 @@ import Show from "components/Appointment/Show";
 import Confirm from "components/Appointment/Confirm";
 import Status from "components/Appointment/Status";
 import Error from "components/Appointment/Error";
+import Form from "components/Appointment/Form";
 
 
 storiesOf("Button", module)
@@ -116,7 +117,7 @@ storiesOf("InterviewerListItem", module)
       id={interviewer.id}
       name={interviewer.name}
       avatar={interviewer.avatar}
-      setInterviewer={event => action("setInterviewer")(interviewer.id)}
+      setInterviewer={action("setInterviewer")(interviewer.id)}
     />
   ));
 
@@ -172,5 +173,17 @@ storiesOf("Appointment", module)
   .add("Error", () => <Error  
     message="Could not delete appointment."
     onClose={action('onClose')}
+  />)
+  .add("Create", () => <Form
+  interviewers={interviewers}
+  onSave={action('onSave')}
+  onCancel={action('onCancel')}
+  />)
+  .add("Edit", () => <Form
+    name='Downshift Markley'
+    interviewers={interviewers}
+    interviewer={2}
+    onSave={action('onSave')}
+    onCancel={action('onCancel')}
   />)
 //
