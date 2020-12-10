6596631b2f52c5dedb73d3c1c981ec7891f5a9cf
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-10 15:38:42 -0800

    use custom hook to conditionally render Show, Empty, Create components
    use empty array for interviewers prop for Create
    do not implement other components yet

--- src/components/Appointment/index.jsx
+++ src/components/Appointment/index.jsx
@@ -1,28 +1,42 @@
 import React from 'react';
 import Header from './Header';
-import Show from './Show'
-import Empty from './Empty'
+import Show from './Show';
+import Empty from './Empty';
+import Form from './Form';
+import Status from './Status';
+import useVisualMode from 'hooks/useVisualMode';
 
 import './styles.scss';
 
 import { action } from "@storybook/addon-actions";
 
 
+const EMPTY = "EMPTY";
+const SHOW = "SHOW";
+const CREATE = "CREATE";
+
 const Appointment = (props) => {
+  const {mode, transition, back} = useVisualMode(props.interview ? SHOW : EMPTY)
+
   return (
     <article className="appointment">
       <Header time={props.time} />
-      {props.interview ? 
-        <Show 
-          student={props.interview.student}
-          interviewer={props.interview.interviewer}
-          onEdit={action('onEdit')}
-          onDelete={action('onDelete')}
-        /> : 
-        <Empty
-          onAdd={action("onAdd")}
-        />
-      }
+      
+      {mode === SHOW && <Show 
+        student={props.interview.student}
+        interviewer={props.interview.interviewer}
+        onEdit={action('onEdit')}
+        onDelete={action('onDelete')}
+      />}
+      {mode === EMPTY && <Empty
+        onAdd={() => transition(CREATE)}
+      />}
+      {mode === CREATE && <Form
+        interviewers={[]}
+        onSave={action('onSave')}
+        onCancel={() => back()}
+      />}
+
     </article>
   )
 }
