106a897b2e5cdc6c87ec3674c52099a250d1f6be
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-09 17:58:51 -0800

    combine Application states into a single state object
    update DayList props to use state.[key]
    write functions for setDay and setDays
    	- have setDays use 'prev' so that useEffect() doesn't depend on state

--- src/components/Application.jsx
+++ src/components/Application.jsx
@@ -71,8 +71,13 @@ const appointments = [
 /****************************************************************************/
 
 const Application = (props) => {
-  const [days, setDays] = useState([]);
-  const [day, setDay] = useState("Monday")
+  const [state, setState] = useState({
+    day: "Monday", 
+    days: [], 
+  })
+
+  const setDay = day => setState({...state, day});
+  const setDays = days => setState(prev => ({...prev, days}));
 
   useEffect(() => {
     axios.get('/api/days').then(res => {
@@ -90,7 +95,11 @@ const Application = (props) => {
           />
           <hr className="sidebar__separator sidebar--centered" />
           <nav className="sidebar__menu">
-            <DayList days={days} selectedDay={day} onChange={setDay} />
+            <DayList 
+              days={state.days}
+              selectedDay={state.day}
+              onChange={setDay} 
+            />
           </nav>
           <img
             className="sidebar__lhl sidebar--centered"
