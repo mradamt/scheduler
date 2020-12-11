25b7cc03a2f86bd5f21bbc0d17c306cfa6b1fb94
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-11 13:33:18 -0800

    refactor to setHistory dynamically and avoid stale state

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,22 +4,21 @@ const useVisualMode = function (initial) {
   const [history, setHistory] = useState([initial]);
 
   const transition = function(mode, replace = false) {
-    const newHistory = [...history]
-    if (replace) {
-      newHistory.shift()
-    };
-    newHistory.unshift(mode)
-    setHistory([...newHistory]);
+    setHistory(prev => {
+      const newHistory = [...prev]
+      if (replace) newHistory.shift();
+      newHistory.unshift(mode)
+      return [...newHistory];
+    })
   }
 
   const back = function() {
-    if (history.length <= 1) {
-      return;
-    }
-
-    const newHistory = [...history]
-    newHistory.shift()
-    setHistory([...newHistory]);
+    setHistory(prev => {
+      if (prev.length <= 1) return;
+      const newHistory = [...prev]
+      newHistory.shift()
+      return [...newHistory];
+    })
   }
 
   const mode = history[0]
