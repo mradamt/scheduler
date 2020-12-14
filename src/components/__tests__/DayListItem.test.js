ee5486c07bb59f0833cb70e3b96693ae535fe74f
Adam Tyler <mr.adamtyler@gmail.com>
2020-12-14 15:22:27 -0800

    Revert "update test scripts' file imports with .jsx file extensions (otherwise tests don't run)"
    This reverts commit dd0e72d0beb9e0fb1aefddfa2fe5ae29c200783f.
    ... .jsx doesn't make a difference, tests are still running

--- src/components/__tests__/DayListItem.test.js
+++ src/components/__tests__/DayListItem.test.js
@@ -2,7 +2,7 @@ import React from "react";
 
 import { render, cleanup } from "@testing-library/react";
 
-import DayListItem from "components/DayListItem.jsx";
+import DayListItem from "components/DayListItem";
 
 afterEach(cleanup);
 
